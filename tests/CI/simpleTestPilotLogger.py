7c19ff9018947931d62bb1d985293df1a76493a0
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-05-10 17:11:50 +0200

    Added queue message comparison

--- tests/CI/simpleTestPilotLogger.py
+++ tests/CI/simpleTestPilotLogger.py
@@ -3,6 +3,7 @@
 import PilotLogger
 import consumeFromQueue
 import os
+import Queue
 
 def main():
   print simpleTestPilotLogger()
@@ -10,13 +11,18 @@ def main():
 def simpleTestPilotLogger():
   uuid ="37356d94-15c6-11e6-a600-606c663dde16"
   filenameUUID = "PilotAgentUUID"
-  expectedMsg='{"status": "Landed", "timestamp": "1462789339.68", "pilotUUID": "37356d94-15c6-11e6-a600-606c663dde16", "minorStatus": "I will send an SOS to the world!", "source": "pilot"}'
+  expectedQueue = Queue()
+  expectedQueue.put('{"status": "Landed", "timestamp": "1462789339.68", "pilotUUID": "37356d94-15c6-11e6-a600-606c663dde16", "minorStatus": "I will send an SOS to the world!", "source": "pilot"}')
+  
   with open ( filenameUUID , 'w' ) as myFile:
     myFile.write( uuid )
 
   os.system("python PilotLogger.py \"I will send an SOS to the world!\"")
-  consumeFromQueue.consume()
-  return "blabla"
+  recvQueue = consumeFromQueue.consume()
+  if expectedQueue == recvQueue:
+    print "ok"
+  else:
+    print "not ok"
 
 if __name__ == '__main__':
   main()
