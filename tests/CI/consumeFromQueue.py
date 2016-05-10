2f4757003815217addbd670737ad26916d3dd114
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-05-10 16:27:19 +0200

    Add consume function to consumeFromQueue file

--- tests/CI/consumeFromQueue.py
+++ tests/CI/consumeFromQueue.py
@@ -9,16 +9,16 @@ class MyListener(stomp.ConnectionListener):
         #sys.stdout.write(message)
         print(message)
 
-host_port  = [('127.0.0.1', int(61614))]
-key_file = 'certificates/client/key.pem'
-cert_file = 'certificates/client/cert.pem'
-ca_certs = 'certificates/testca/cacert.pem'
-conn = stomp.Connection(host_and_ports=host_port, use_ssl = True)
-conn.set_ssl(for_hosts=host_port, key_file = key_file,cert_file = cert_file, ca_certs=ca_certs)
-conn.set_listener('', MyListener())
-conn.start()
-conn.connect()
-conn.subscribe(destination='/queue/test', id=1, ack='auto')
-#conn.connect('ala', 'ala', wait=True)
-conn.disconnect()
+def consume():
+  host_port  = [('127.0.0.1', int(61614))]
+  key_file = 'certificates/client/key.pem'
+  cert_file = 'certificates/client/cert.pem'
+  ca_certs = 'certificates/testca/cacert.pem'
+  conn = stomp.Connection(host_and_ports=host_port, use_ssl = True)
+  conn.set_ssl(for_hosts=host_port, key_file = key_file,cert_file = cert_file, ca_certs=ca_certs)
+  conn.set_listener('', MyListener())
+  conn.start()
+  conn.connect()
+  conn.subscribe(destination='/queue/test', id=1, ack='auto')
+  conn.disconnect()
 
