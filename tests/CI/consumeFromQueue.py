193e0e7e630f52b159465a359872a605b8196ded
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-05-10 17:02:35 +0200

    Add return of message queue taken from the RabbitMq queue

--- tests/CI/consumeFromQueue.py
+++ tests/CI/consumeFromQueue.py
@@ -1,30 +1,36 @@
-import time
-import sys
 import stomp
+import Queue
 
 class MyListener(stomp.ConnectionListener):
-    def on_error(self, headers, message):
-        print('received an error "%s"' % message)
-    def on_message(self, headers, message):
-        print(message)
+  def __init__( self ):
+    self.messages = Queue.Queue()
+  def on_error(self, headers, message):
+    print('received an error "%s"' % message)
+  def on_message(self, headers, message):
+    self.messages.put(message)
+    print message
 
 def main():
   consume()
 
 def consume():
+  '''
+  Returns:
+    Queue:
+  '''
   host_port  = [('128.142.242.99', int(61614))]
   key_file = 'certificates/client/key.pem'
   cert_file = 'certificates/client/cert.pem'
   ca_certs = 'certificates/testca/cacert.pem'
   conn = stomp.Connection(host_and_ports=host_port, use_ssl = True)
   conn.set_ssl(for_hosts=host_port, key_file = key_file,cert_file = cert_file, ca_certs=ca_certs)
-  conn.set_listener('', MyListener())
+  listener = MyListener()
+  conn.set_listener('', listener)
   conn.start()
   conn.connect(wait=True)
   conn.subscribe(destination='/queue/test', id=1, ack='auto')
   conn.disconnect()
-  #while 1:
-    #pass
+  return listener.messages
 
 if __name__ == '__main__':
   main()
