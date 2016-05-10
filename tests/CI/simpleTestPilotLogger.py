b08b1af299f6ca01cd25cf3f46e931e3a40fe133
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-05-10 17:40:37 +0200

    Change Queue to list and add removing timestamp element

--- tests/CI/simpleTestPilotLogger.py
+++ tests/CI/simpleTestPilotLogger.py
@@ -3,7 +3,6 @@
 import PilotLogger
 import consumeFromQueue
 import os
-import Queue
 
 def main():
   print simpleTestPilotLogger()
@@ -11,17 +10,22 @@ def main():
 def simpleTestPilotLogger():
   uuid ="37356d94-15c6-11e6-a600-606c663dde16"
   filenameUUID = "PilotAgentUUID"
-  expectedQueue = Queue.Queue()
-  expectedQueue.put('{"status": "Landed", "timestamp": "1462789339.68", "pilotUUID": "37356d94-15c6-11e6-a600-606c663dde16", "minorStatus": "I will send an SOS to the world!", "source": "pilot"}')
-  
+  expectedMsgs = []
+  expectedMsgs.append('{"status": "Landed", "timestamp": "1462789339.68", "pilotUUID": "37356d94-15c6-11e6-a600-606c663dde16", "minorStatus": "I will send an SOS to the world!", "source": "pilot"}')
+
   with open ( filenameUUID , 'w' ) as myFile:
     myFile.write( uuid )
 
   os.system("python PilotLogger.py \"I will send an SOS to the world!\"")
-  recvQueue = consumeFromQueue.consume()
-  if expectedQueue == recvQueue:
+  recvMsgs = consumeFromQueue.consume()
+  #we get rid of the timestamp
+  expectedMsgs = [x.pop("timestamp",None) for x in expectedMsgs]
+  recvMsgs = [x.pop("timestamp",None) for x in expectedMsgs]
+  if expectedMsgs == recvMsgs:
     print "ok"
   else:
+    print expectedMsgs
+    print recvMsgs
     print "not ok"
 
 if __name__ == '__main__':
