a82d3bf7a7e30ff1beb7eb15d0c6fe3ae9c0ee8d
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-05-10 16:23:50 +0200

    More functionality to simpleTestPilotLogger.py

--- tests/CI/simpleTestPilotLogger.py
+++ tests/CI/simpleTestPilotLogger.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 
 import PilotLogger
-import stomp
+import consumeFromQueue
+import os
+
 def main():
   print simpleTestPilotLogger()
 
 def simpleTestPilotLogger():
+  uuid ="37356d94-15c6-11e6-a600-606c663dde16"
+  filenameUUID = "PilotAgentUUID"
+  expectedMsg='{"status": "Landed", "timestamp": "1462789339.68", "pilotUUID": "37356d94-15c6-11e6-a600-606c663dde16", "minorStatus": "I will send an SOS to the world!", "source": "pilot"}'
+  with open ( filenameUUID , 'w' ) as myFile:
+    myFile.write( uuid )
+
+  os.system("python PilotLogger.py \"I will send an SOS to the world!\"")
+  consumeFromQueue.consume()
   return "blabla"
-#PILOT_UUID="37356d94-15c6-11e6-a600-606c663dde16  
-  #echo -n "$PILOT_UUID" > "PilotAgentUUID"
-  #EXPECTED_MSG='{"status": "Landed", "timestamp": "1462789339.68", "pilotUUID": "37356d94-15c6-11e6-a600-606c663dde16", "minorStatus": "I will send an SOS to the world!", "source": "pilot"}'
-  #python PilotLogger.py "I will send an SOS to the world!"
-  #RECV_MSG=$(python consumeFromQueue.py)
-  #RESULT=$(python compare.py "$RECV_MSG" "$EXPECTED_MSG")
-  
 
 if __name__ == '__main__':
   main()
