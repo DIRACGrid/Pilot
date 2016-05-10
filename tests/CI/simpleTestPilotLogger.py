da136cc81d3008450697bb99da688f9f1904386b
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-05-10 16:12:05 +0200

    Small changes in simpleTestPilotLogger

--- tests/CI/simpleTestPilotLogger.py
+++ tests/CI/simpleTestPilotLogger.py
@@ -3,10 +3,11 @@
 import PilotLogger
 import stomp
 def main():
-  return simpleTestPilotLogger()
+  print simpleTestPilotLogger()
 
 def simpleTestPilotLogger():
   return "blabla"
+#PILOT_UUID="37356d94-15c6-11e6-a600-606c663dde16  
   #echo -n "$PILOT_UUID" > "PilotAgentUUID"
   #EXPECTED_MSG='{"status": "Landed", "timestamp": "1462789339.68", "pilotUUID": "37356d94-15c6-11e6-a600-606c663dde16", "minorStatus": "I will send an SOS to the world!", "source": "pilot"}'
   #python PilotLogger.py "I will send an SOS to the world!"
