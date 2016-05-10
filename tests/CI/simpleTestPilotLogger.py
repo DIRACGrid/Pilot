430e68d5ba69bb6d7fbb55eb606c44b260a23d5d
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-05-10 15:55:22 +0200

    Add simpleTestPilotLogger and rename consumer to consumeFromQueue.py

---
+++ tests/CI/simpleTestPilotLogger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import PilotLogger
+import stomp
+def main():
+  return simpleTestPilotLogger()
+
+def simpleTestPilotLogger():
+  return "blabla"
+  #echo -n "$PILOT_UUID" > "PilotAgentUUID"
+  #EXPECTED_MSG='{"status": "Landed", "timestamp": "1462789339.68", "pilotUUID": "37356d94-15c6-11e6-a600-606c663dde16", "minorStatus": "I will send an SOS to the world!", "source": "pilot"}'
+  #python PilotLogger.py "I will send an SOS to the world!"
+  #RECV_MSG=$(python consumeFromQueue.py)
+  #RESULT=$(python compare.py "$RECV_MSG" "$EXPECTED_MSG")
+  
+
+if __name__ == '__main__':
+  main()
