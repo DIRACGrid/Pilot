32f0539bc5f3746bea9e8d9234572cd2dbe6ef02
Wojciech Krzemien <wojciech.krzemien@ncbj.gov.pl>
2016-11-12 16:31:09 +0100

    Add checks if arguments are single word and limit to 200 charactes for message
    Also reformatof the help and add some examples.

--- Pilot/PilotLogger.py
+++ Pilot/PilotLogger.py
@@ -223,31 +223,50 @@ def main():
       already generated and stored into some file.
   """
 
-  parser = argparse.ArgumentParser(description="command line interface to send logs to MQ system.")
+  def singleWord(arg):
+    if len(arg.split()) !=1:
+      msg = 'argument must be single word'
+      raise argparse.ArgumentTypeError(msg)
+    return arg
+
+  parser = argparse.ArgumentParser(description="command line interface to send logs to MQ system.",
+                                  formatter_class=argparse.RawTextHelpFormatter,
+                                  epilog=
+                                    'examples:\n'
+                                    +'                   python PilotLogger.py InstallDIRAC installing info My message\n'
+                                    +'                   python PilotLogger.py InstallDIRAC installing debug Debug message\n'
+                                    +'                   python PilotLogger.py "My message"\n'
+                                    +'                   python PilotLogger.py "My message" --output myFileName\n'
+                                  )
   parser.add_argument('source',
+                      type = singleWord,
                       nargs='?',
                       default ='unspecified',
-                      help='''Source of the message e.g. "InstallDIRAC". It must be one word.
-                            If not specified it is set to "unspecified".''')
+                      help='Source of the message e.g. "InstallDIRAC". It must be one word. '
+                           +'If not specified it is set to "unspecified".')
   parser.add_argument('phase',
+                      type = singleWord,
                       nargs='?',
                       default ='unspecified',
-                      help='''Phase of the process e.g. "fetching". It must be one word.
-                            If not specified it is set to "unspecified".''')
+                      help='Phase of the process e.g. "fetching". It must be one word. '
+                            +'If not specified it is set to "unspecified".')
   parser.add_argument('status',
                       nargs = '?',
                       choices = PilotLogger.STATUSES,
                       default = 'info',
-                      help = 'Allowed values are: '+ ', '.join(PilotLogger.STATUSES)+'''.
-                      If not specified it is set to "info".''',
+                      help = 'Allowed values are: '+ ', '.join(PilotLogger.STATUSES)
+                      +'. If not specified it is set to "info".',
                       metavar='status ')
   parser.add_argument('message',
                       nargs='+',
                       help='Human readable content of the message. ')
   parser.add_argument('--output',
-                      help = '''Log the content to the specified file
-                              instead of sending it to the Message Queue server.''')
+                      help = 'Log the content to the specified file'
+                             +' instead of sending it to the Message Queue server.')
   args = parser.parse_args()
+
+  if len(" ".join(args.message)) >= 200:
+    raise argparse.ArgumentTypeError('message must be less than 200 characters')
   logger = PilotLogger()
   if args.output:
     print args.output
