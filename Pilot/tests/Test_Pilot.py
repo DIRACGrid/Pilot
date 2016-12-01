8a3110e1e0b6ba71acfdf1aaf539c073a6f780b3
Andrew McNab <andrew.mcnab@cern.ch>
2016-12-01 17:10:40 +0100

    Fixes for Jenkins lint

--- Pilot/tests/Test_Pilot.py
+++ Pilot/tests/Test_Pilot.py
@@ -9,7 +9,7 @@ import sys
 import os
 
 from Pilot.pilotTools import PilotParams
-from Pilot.pilotCommands import GetPilotVersion, CheckWorkerNode, ConfigureSite, NagiosProbes
+from Pilot.pilotCommands import CheckWorkerNode, ConfigureSite, NagiosProbes
 
 class PilotTestCase( unittest.TestCase ):
   """ Base class for the Agents test cases
@@ -17,16 +17,16 @@ class PilotTestCase( unittest.TestCase ):
   def setUp( self ):
     # Define a local file for test, and all the necessary parameters
     with open ( 'pilot.json', 'w' ) as fp:
-      json.dump( {'Setups':{'TestSetup':{'Commands':{'cetype1':['x', 'y', 'z'], 
-                                                     'cetype2':['d', 'f']}, 
+      json.dump( {'Setups':{'TestSetup':{'Commands':{'cetype1':['x', 'y', 'z'],
+                                                     'cetype2':['d', 'f']},
                                          'CommandExtensions':['TestExtension'],
                                          'NagiosProbes':'Nagios1,Nagios2',
                                          'NagiosPutURL':'https://127.0.0.2/',
                                          'Version':['v1r1','v2r2']}},
                   'CEs':{'grid1.example.com':{'GridCEType':'cetype1','Site':'site.example.com'}},
-                  'DefaultSetup':'TestSetup'}, 
+                  'DefaultSetup':'TestSetup'},
                  fp )
-                
+
     sys.argv[1:] = ['--Name', 'grid1.example.com']
 
     self.pp = PilotParams()
@@ -37,33 +37,42 @@ class PilotTestCase( unittest.TestCase ):
     except IOError:
       pass
 
-
 class CommandsTestCase( PilotTestCase ):
+  """ Test case for each pilot command
+  """
 
   def test_InitJSON( self ):
+    """ Test the pilot.json parsing
+    """
     self.assertEqual( self.pp.commands, ['x', 'y', 'z'] )
     self.assertEqual( self.pp.commandExtensions, ['TestExtension'] )
-    
+
   def test_CheckWorkerNode ( self ):
+    """ Test CheckWorkerNode command
+    """
     CheckWorkerNode( self.pp )
-        
+
   def test_ConfigureSite ( self ):
+    """ Test ConfigureSite command
+    """
     self.pp.configureScript = 'echo'
     ConfigureSite( self.pp )
-        
+
   def test_NagiosProbes ( self ):
+    """ Test NagiosProbes command
+    """
     nagios = NagiosProbes( self.pp )
 
     with open ( 'Nagios1', 'w') as fp:
       fp.write('#!/bin/sh\necho 123\n')
-      
+
     os.chmod( 'Nagios1', stat.S_IRWXU )
 
     with open ( 'Nagios2', 'w') as fp:
       fp.write('#!/bin/sh\necho 567\n')
-      
+
     os.chmod( 'Nagios2', stat.S_IRWXU )
-  
+
     nagios.execute()
 
     self.assertEqual( nagios.nagiosProbes, ['Nagios1', 'Nagios2'] )
