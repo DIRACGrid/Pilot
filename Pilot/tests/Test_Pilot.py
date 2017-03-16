aed01b5ce173a624bdb90769befa7fd94230fb0d
Federico Stagni <federico.stagni@cern.ch>
2017-03-16 09:56:58 +0100

    fix

--- Pilot/tests/Test_Pilot.py
+++ Pilot/tests/Test_Pilot.py
@@ -22,7 +22,9 @@ class PilotTestCase( unittest.TestCase ):
                                          'CommandExtensions':'TestExtension1,TestExtension2',
                                          'NagiosProbes':'Nagios1,Nagios2',
                                          'NagiosPutURL':'https://127.0.0.2/',
-                                         'Version':['v1r1, v2r2']}},
+                                         'Version':'v1r1, v2r2'
+                                        }
+                           },
                   'CEs':{'grid1.example.com':{'GridCEType':'cetype1','Site':'site.example.com'}},
                   'DefaultSetup':'TestSetup'},
                  fp )
