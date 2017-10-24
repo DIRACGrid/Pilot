663b3f7b21978d9c0284e1e7688ce709c13fa479
Andrew McNab <andrew.mcnab@cern.ch>
2017-10-24 11:22:44 +0200

    Remove FIXME

--- Pilot/pilotTools.py
+++ Pilot/pilotTools.py
@@ -508,12 +508,12 @@ class PilotParams( object ):
                      ( 'x:', 'execute=', 'Execute instead of JobAgent' ),
                      ( 'y:', 'CEType=', 'CE Type (normally InProcess)' ),
                      ( 'z',  'pilotLogging', 'Activate pilot logging system' ),
-                     ( 'C:', 'configurationServer=', 'Configuration servers to use' ), # FIXME: -C is used twice! Which one to keep?
+                     ( 'C:', 'configurationServer=', 'Configuration servers to use' ),
                      ( 'D:', 'disk=', 'Require at least <space> MB available' ),
                      ( 'E:', 'commandExtensions=', 'Python modules with extra commands' ),
                      ( 'F:', 'pilotCFGFile=', 'Specify pilot CFG file' ),
                      ( 'G:', 'Group=', 'DIRAC Group to use' ),
-                     ( 'K:', 'certLocation=', 'Specify server certificate location' ), # FIXME: -C is used twice! Which one to keep?
+                     ( 'K:', 'certLocation=', 'Specify server certificate location' ),
                      ( 'M:', 'MaxCycles=', 'Maximum Number of JobAgent cycles to run' ),
                      ( 'N:', 'Name=', 'CE Name' ),
                      ( 'O:', 'OwnerDN=', 'Pilot OwnerDN (for private pilots)' ),
