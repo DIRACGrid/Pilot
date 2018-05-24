7a524c5897e67838ec5d0afcc3bd7636cb088996
Wojciech Jan Krzemien <wojciech.krzemien@cern.ch>
2018-05-24 17:00:58 +0200

    Playing with stomp installation in pilot_ci.sh

--- tests/CI/pilot_ci.sh
+++ tests/CI/pilot_ci.sh
@@ -207,7 +207,8 @@ function installStompIfNecessary()
           type yum > /dev/null 2>&1 || { echo >&2 "yum installer is required. Aborting"; exit 1; }
           yum -y install python-pip
       fi
-      #pip install  --user --upgrade pip 
+      pip install --user --upgrade pip 
+      pip install --user stomp 
       #~/.local/bin/pip2.7 install --user 'stomp.py==4.1.11'
   fi
   #stomp should be installed now
