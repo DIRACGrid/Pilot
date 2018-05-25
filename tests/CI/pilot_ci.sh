c5e94ed728198e9dcbeeddcf3a2334f95acf8770
Wojciech Jan Krzemien <wojciech.krzemien@cern.ch>
2018-05-25 11:49:09 +0200

    change default pilot.json

--- tests/CI/pilot_ci.sh
+++ tests/CI/pilot_ci.sh
@@ -83,7 +83,8 @@ function PilotInstall(){
 
   #get the configuration file (from an VO extension, if it exists)
   pilot="Pilot"
-  cp $TESTCODE/$VO$pilot/tests/CI/pilot.json .
+  #cp $TESTCODE/$VO$pilot/tests/CI/pilot.json .
+  cp $TESTCODE/$VO$pilot/tests/CI/pilotLocalLogger.json ./pilot.json
   # and adapt it
   sed -i s/VAR_JENKINS_SITE/$JENKINS_SITE/g pilot.json
   sed -i s/VAR_JENKINS_CE/$JENKINS_CE/g pilot.json
