e78828d6bf5ed0385957e91e72036dc64ac57000
Federico Stagni <federico.stagni@cern.ch>
2018-11-14 11:30:21 +0100

    warn about missing version

--- Pilot/pilotTools.py
+++ Pilot/pilotTools.py
@@ -820,14 +820,15 @@ class PilotParams(object):
 
     # Version
     # There may be a list of versions specified (in a string, comma separated). We just want the first one.
+    dVersion = None
     try:
       dVersion = [dv.strip() for dv in self.pilotJSON['Setups'][self.setup]['Version'].split(',', 1)]
     except KeyError:
       try:
         dVersion = [dv.strip() for dv in self.pilotJSON['Setups']['Defaults']['Version'].split(',', 1)]
       except KeyError:
-        dVersion = None
-    if dVersion:
+        self.log.warn("Could not find a version in the JSON file configuration")
+    if dVersion is not None:
       self.releaseVersion = str(dVersion[0])
     self.log.debug("Version: %s -> %s" % (dVersion, self.releaseVersion))
 
