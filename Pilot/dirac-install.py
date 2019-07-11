a9331bbfe8bf7716ac008ef397ffeee069b3f746
Federico Stagni <federico.stagni@cern.ch>
2019-07-11 17:36:22 +0200

    removed unicode_literals

--- Pilot/dirac-install.py
+++ Pilot/dirac-install.py
@@ -128,7 +128,7 @@ You can use install.cfg configuration file::
 
 #  pylint: skip-file
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function
 
 import sys
 import os
