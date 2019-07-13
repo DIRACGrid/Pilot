c8b5b04b1ed1871e619dc29e55243cea198339fe
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2019-07-13 17:07:19 +0200

    Add installation of requests to the CI test

--- tests/CI/pilot_ci.sh
+++ tests/CI/pilot_ci.sh
@@ -92,7 +92,7 @@ function PilotInstall(){
   sed -i "s#VAR_USERDN#$DIRACUSERDN#g" pilot.json
 
   prepareForPilot
-  #installStompRequestsIfNecessary
+  installStompRequestsIfNecessary
   #preparePythonEnvironment
   python PilotLoggerTools.py PilotUUID
   python PilotLogger.py "Hello I am THE best pilot"
