b76b342e02326cacc2e664e3ac5f1798ac207ade
Federico Stagni <federico.stagni@cern.ch>
2019-09-25 18:04:11 +0200

    pylint 3k

--- runPylint.sh
+++ runPylint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-find . -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.pylintrc --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
+if [[ "${CHECK}" == "pylintPY3K"  ]]; then
+  find . -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.pylintrc3k --py3k --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
+else
+  find . -name "*.py" -and -not -name 'pep8_*' -exec pylint -E --rcfile=.pylintrc --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} +
+fi
