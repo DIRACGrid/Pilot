b66ac487b3c399b74731efdf1d47034d347136bc
Federico Stagni <federico.stagni@cern.ch>
2020-01-20 17:14:23 +0100

    fix version and corrected typo

--- tests/upload.sh
+++ tests/upload.sh
@@ -9,4 +9,4 @@ cp tests/pilot.json $tmpdir
 # create the tar and upload
 cd $tmpdir
 tar -cf pilot.tar *.py
-( tar -cf - pilot.tar pilot.json ) | ssh lhcbprod@lxplus.cern.ch "cd /eos/project/d/diracgrid/www/tars/Pilot/DIRAC/${1}/ && tar -xvf - "
+( tar -cf - pilot.tar pilot.json ) | ssh uploaduser@lxplus.cern.ch "cd /eos/project/d/diracgrid/www/tars/Pilot/DIRAC/${1}/ && tar -xvf - "
