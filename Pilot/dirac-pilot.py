3b6bf8a929728c992a8d3343d3c60f51b6a2111a
martynia <janusz.martyniak@googlemail.com>
2022-09-06 15:49:17 +0200

    fix: clarify comments

--- Pilot/dirac-pilot.py
+++ Pilot/dirac-pilot.py
@@ -28,13 +28,14 @@ import os
 import sys
 import time
 
+############################
+# python 2 -> 3 "hacks"
+
 try:
     from cStringIO import StringIO
 except ImportError:
     from io import StringIO
 
-############################
-# python 2 -> 3 "hacks"
 try:
     from Pilot.pilotTools import (
         Logger,
