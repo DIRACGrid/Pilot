ade5910a5b598e9c84f42c7981103df8142fad98
martynia <janusz.martyniak@googlemail.com>
2023-02-01 12:15:40 +0100

    feat: add test based on voms-proxy-fake + a set of fake certificates

--- tests/CI/Test_proxyTools.py
+++ tests/CI/Test_proxyTools.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
-import os
-import sys
-import string
-import random
 import subprocess
+import shlex
+import sys
+import os
+import shutil
 
 try:
     from Pilot.proxyTools import getVO, parseASN1
@@ -22,10 +22,13 @@ except ImportError:
 class TestProxyTools(unittest.TestCase):
     def test_getVO(self):
         vo = "unknown"
-        cert = "x509up_u53911"
+        cert = "x509_uFAKE"
+        ret = self.__createFakeProxy(cert)
+        self.assertEqual(ret, 0)
         with open(cert, "rb") as fp:
             vo = getVO(fp.read())
-        self.assertEqual(vo, "gridpp")
+        os.remove(cert)
+        self.assertEqual(vo, "fakevo")
 
     @patch("Pilot.proxyTools.Popen")
     def test_getVOPopenFails(self, popenMock):
@@ -35,11 +38,13 @@ class TestProxyTools(unittest.TestCase):
         """
         popenMock.return_value.returncode = -1
         popenMock.return_value.communicate.return_value = ("output", "error")
-        cert = "x509up_u53911"
+        cert = "x509_uFAKE"
+        ret = self.__createFakeProxy(cert)
+        self.assertEqual(ret, 0)
         msg = "Error when invoking openssl X509"
         with open(cert, "rb") as fp:
             data = fp.read()
-
+        os.remove(cert)
         with self.assertRaises(Exception) as exc:
             vo = getVO(data)
 
@@ -55,7 +60,7 @@ class TestProxyTools(unittest.TestCase):
 
     @patch("Pilot.proxyTools.Popen")
     def test_parseASN1Fails(self, popenMock):
-        """ Should raise an exception when Popen return code is !=0 """
+        """Should raise an exception when Popen return code is !=0"""
 
         popenMock.return_value.returncode = -1
         popenMock.return_value.communicate.return_value = ("output", "error")
@@ -75,5 +80,57 @@ class TestProxyTools(unittest.TestCase):
             res = parseASN1("Anything")
         self.assertEqual(str(exc.exception), msg)
 
+    def test_createFakeProxy(self):
+        """Just test if a proxy is created"""
 
+        ret = self.__createFakeProxy("x509_uFAKE")
+        self.assertEqual(ret, 0)
+        os.remove("x509_uFAKE")
 
+    def __createFakeProxy(self, proxyFile):
+        """
+        Create a fake proxy locally.
+        """
+
+        basedir = os.path.dirname(__file__)
+        shutil.copy(basedir + "/certs/user/userkey.pem", basedir + "/certs/user/userkey400.pem")
+        os.chmod(basedir + "/certs/user/userkey400.pem", 0o400)
+        ret = self.createFakeProxy(
+            basedir + "/certs/user/usercert.pem",
+            basedir + "/certs/user/userkey400.pem",
+            "fakeserver.cern.ch:15000",
+            "fakevo",
+            basedir + "/certs//host/hostcert.pem",
+            basedir + "/certs/host/hostkey.pem",
+            basedir + "/certs/ca",
+            proxyFile,
+        )
+        os.remove(basedir + "/certs/user/userkey400.pem")
+        return ret
+
+    def createFakeProxy(self, usercert, userkey, serverURI, vo, hostcert, hostkey, CACertDir, proxyfile):
+        """
+        voms-proxy-fake --cert usercert.pem
+                        --key userkey.pem
+                        -rfc
+                        -fqan "/fakevo/Role=user/Capability=NULL"
+                        -uri fakeserver.cern.ch:15000
+                        -voms fakevo
+                        -hostcert hostcert.pem
+                        -hostkey hostkey.pem
+                        -certdir ca
+        """
+        opt = (
+            '--cert %s --key %s -rfc -fqan "/fakevo/Role=user/Capability=NULL" -uri %s -voms %s -hostcert %s'
+            "  -hostkey %s  -certdir %s -out %s"
+            % (usercert, userkey, serverURI, vo, hostcert, hostkey, CACertDir, proxyfile)
+        )
+        proc = subprocess.Popen(
+            shlex.split("voms-proxy-fake " + opt),
+            bufsize=1,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            universal_newlines=True,
+        )
+        proc.communicate()
+        return proc.returncode
