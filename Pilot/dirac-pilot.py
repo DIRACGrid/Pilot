1c2eecf099b76936b75e8502a11b9b825ce60e63
Janusz Martyniak <janusz.martyniak@googlemail.com>
2024-01-23 13:01:32 +0100

    feat: add a flag to vary a remote logger buffer size

--- Pilot/dirac-pilot.py
+++ Pilot/dirac-pilot.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         if not sys.stdin.isatty():
             receivedContent = sys.stdin.read()
         log = RemoteLogger(
-            pilotParams.loggerURL, "Pilot", pilotUUID=pilotParams.pilotUUID, debugFlag=pilotParams.debugFlag
+            pilotParams.loggerURL, "Pilot", bufsize=pilotParams.loggerBufsize, pilotUUID=pilotParams.pilotUUID, debugFlag=pilotParams.debugFlag
         )
         log.info("Remote logger activated")
         log.buffer.write(receivedContent)
